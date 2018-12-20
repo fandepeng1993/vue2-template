e022858975917be58d409c30b845f4867204304a
fandepeng <depeng.fan@eoitek.com>
2018-12-20 10:11:18 +0800

    first

---
+++ src/components/ceair-aside/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <el-menu
+    :router="false"
+    style="border-right: 0"
+    default-active="0"
+    class="el-menu-vertical-demo"
+    @open="handleOpen"
+    @close="handleClose"
+    @select="activetab"
+    ref="elMenu"
+    background-color="#545c64"
+    text-color="#fff"
+    :unique-opened="true"
+    active-text-color="#ffd04b">
+    <el-menu-item index="0">
+      <i class="el-icon-menu"></i>
+      <span slot="title">首页</span>
+    </el-menu-item>
+    <el-submenu index="1">
+      <template slot="title"><i class="el-icon-message"></i>导航一</template>
+      <el-menu-item index="1-1">选项1</el-menu-item>
+      <el-menu-item index="1-2">选项2</el-menu-item>
+      <el-menu-item index="1-3">选项3</el-menu-item>
+      <el-submenu index="1-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-submenu index="2">
+      <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+      <el-menu-item index="2-1">选项1</el-menu-item>
+      <el-menu-item index="2-2">选项2</el-menu-item>
+      <el-menu-item index="2-3">选项3</el-menu-item>
+      <el-submenu index="2-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-submenu index="3">
+      <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+      <el-menu-item index="3-1">选项1</el-menu-item>
+      <el-menu-item index="3-2">选项2</el-menu-item>
+      <el-menu-item index="3-3">选项3</el-menu-item>
+      <el-submenu index="3-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  created () {
+    setTimeout(() => {
+
+    })
+  },
+  methods: {
+    activetab (index) {
+      if(index=='1-1') {
+        console.log(111)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/home/component.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="self-component">
+      <div>asdasdafdsdfsdf</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'self-component',
+  props: {
+    myobject: {
+      type: Object
+    }
+  },
+  data () {
+    return {}
+  },
+  created(){
+    console.log(12312312)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
